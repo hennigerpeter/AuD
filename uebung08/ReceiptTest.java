2e0ea39b1414d56068bef0bfc076550085a62d50
khalie <henniger.peter@gmail.com>
2016-12-13 22:25:17 +0100

    ReceiptTest Class

--- uebung08/ReceiptTest.java
+++ uebung08/ReceiptTest.java
@@ -4,28 +4,54 @@ public class ReceiptTest {
 	 * different failure conditions to be found. (you need at least 9 to get full points)
 	 */
 
+	// NegativeArraySizeException
 	public static void triggerException1() {
 		// TODO
+		int nulltest = Integer.MIN_VALUE;
+		Receipt test = new Receipt(nulltest);
 	}
-
+	
+	// ArrayIndexOutOfBoundsException
 	public static void triggerException2() {
 		// TODO
+		int nulltest = 0;
+		Receipt test = new Receipt(nulltest);
+		test.register("", "");
 	}
 
+	// NumberFormatException
 	public static void triggerException3() {
 		// TODO
+		int nulltest = 4;
+		Receipt test = new Receipt(nulltest);
+		test.register("", "");
 	}
 
+	// ArrayIndexOutOfBoundsException
 	public static void triggerException4() {
 		// TODO
+		int nulltest = 4;
+		Receipt test = new Receipt(nulltest);
+		test.register("                                                               ", "                                                               ");
+		String tes2 = test.getLastProduct();
 	}
 
+	// NullPointerException
 	public static void triggerException5() {
 		// TODO
+		int nulltest = 4;
+		Receipt test = new Receipt(nulltest);
+		test.register("                                                               ", "                                                               ");
+		int i = test.sum();
 	}
 
 	public static void triggerException6() {
 		// TODO
+		int nulltest = 4;
+		Receipt test = new Receipt(nulltest);
+		int bla = Integer.MAX_VALUE;
+		test.register("Schnitzel", "9999999999999999999999999" );
+		int i = test.sum();
 	}
 
 	public static void triggerException7() {
