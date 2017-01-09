6f93e62359a75ee0ec7c2d1ff667335a65db3ecb
Peter Henniger <henniger.peter@gmail.com>
2017-01-09 10:09:58 +0100

    unimodale suche completed

--- uebung09/UnimodaleSuche.java
+++ uebung09/UnimodaleSuche.java
@@ -1,35 +1,69 @@
 import java.util.Comparator;
 
-public abstract class UnimodaleSuche<T extends Comparable<T>> implements UnimodaleListe<T> {
+public abstract class UnimodaleSuche<T extends Comparable<T>> {
 
 	public static <T> T suche(UnimodaleListe<T> ulldComparator, int von, int bis, Comparator<T> cldc) {
-		if (ulldComparator == null)
+		if (ulldComparator == null || von < 0 || bis < 0 || von > bis)
 			return null;
+		else {
+			// Basisfall = Feld gefunden
+			if (bis == von)
+				return ulldComparator.hole(von);
+			else {
+				// Fuer den Fall dass durch die Rundung die falsche Seite
+				// betrachtet
+				// wird, muss beidseitig geprueft werden
+								
+				T halfAndOne = ulldComparator.hole((von+bis)/2+1);
+				T half = ulldComparator.hole((von+bis)/2);
 
-		// Basisfall = Feld gefunden
-		if (cldc.compare(ulldComparator.hole(von), ulldComparator.hole(bis)) == 0)
-			return ulldComparator.hole(von);
+				int comparisonVH = cldc.compare(half, halfAndOne);
 
-		int half = (von + bis) / 2;
-		int comparisonVH = cldc.compare(ulldComparator.hole(von), ulldComparator.hole(half));
-
-		// VON < HALF: Suche in der zweiten haelfte
-		if (comparisonVH < 0)
-			return suche(ulldComparator, half, bis, cldc);
-		// VON > HALF: Suche in der ersten haelfte
-		else // if (comparisonVH > 0)
-			return suche(ulldComparator, von, half, cldc);
-
+				// Fall 1: Beide Felder identisch
+				if (comparisonVH == 0)
+					return half;
+				else {
+					// VON < HALF: Suche in der zweiten haelfte
+					if (comparisonVH < 0) {
+						if (bis != von + 1)
+							return suche(ulldComparator, (von+bis)/2+1, bis, cldc);
+						else
+							return halfAndOne;
+					}
+					// VON > HALF: Suche in der ersten haelfte
+					else {// if (comparisonVH > 0)
+						if (bis != von + 1)
+							return suche(ulldComparator, von, (von+bis)/2, cldc);
+						else
+							return half;
+					}
+				}
+			}
+		}
 	}
 
-	public static <T> T suche(UnimodaleListe<T> ulldComparable, int von, int bis) {
-		if (ulldComparable == null)
+	public static <T extends Comparable<T>> T suche(UnimodaleListe<T> ulldComparable, int von, int bis) {
+		if (ulldComparable == null || von < 0 || bis < 0 || von > bis)
 			return null;
-
-		if (ulldComparable.hole(von).equals(ulldComparable.hole(bis)))
-			return ulldComparable.hole(von);
-
-		return null;
+		else {
+			if (bis == von)
+				return ulldComparable.hole(von);
+			else {
+				T halfAndOne = ulldComparable.hole((bis+von)/2+1); 
+				T half = ulldComparable.hole((bis+von)/2);
+				
+				if (half.compareTo(halfAndOne) == 0) 
+					return half; 
+			 	else if (half.compareTo(halfAndOne) < 0)
+				{ 
+					if (bis != von + 1) 
+						return suche(ulldComparable, (von+bis)/2+1, bis); else return halfAndOne; 
+				} else if (bis != von + 1) 
+					return suche(ulldComparable, von, (von+bis)/2); else return half; 
+				
+			}
+		}
+		
 	}
 
 }
