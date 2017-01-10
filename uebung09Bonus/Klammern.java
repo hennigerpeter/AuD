cab26f3528c0e81c5eef213197f50220e693064c
Peter Henniger <Peter.Henniger@proleit.com>
2017-01-10 22:44:49 +0100

    1/5 public Tests

--- uebung09Bonus/Klammern.java
+++ uebung09Bonus/Klammern.java
@@ -12,20 +12,28 @@ public class Klammern {
 
 		for (int i = 1; i < input.length(); i++) {
 			String string1 = input.substring(0, i) + klammeraussen(input.substring(i));
-			String string2 = klammeraussen(input.substring(0, i)) + input.substring(i, input.length());
+			String string2 = klammeraussen(input.substring(0, i)) + input.substring(i);
 
-			for (int j = 1; j < string1.length(); j++) {
+			if (valid(string1))
+				mylist.add(string1);
+			if (valid(string2))
+				mylist.add(string2);
+			
+			for (int j = 0; j < string1.length(); j++) {
 				String string3 = string1.substring(0, j) + klammeraussen(string1.substring(j));
 				String string4 = klammeraussen(string1.substring(0, j)) + string1.substring(j, string1.length());
 
 				if (valid(string3))
 					mylist.add(string3);
+				if (valid(klammeraussen(string3)))
+					mylist.add(klammeraussen(string3));
 				if (valid(string4))
 					mylist.add(string4);
-
+				if (valid(klammeraussen(string4)))
+					mylist.add(klammeraussen(string4));
 			}
 
-			for (int k = 1; k < string2.length(); k++) {
+			for (int k = 0; k < string2.length(); k++) {
 				String string3 = string2.substring(0, k) + klammeraussen(string2.substring(k));
 				String string4 = klammeraussen(string2.substring(0, k)) + string2.substring(k, string2.length());
 
@@ -37,18 +45,27 @@ public class Klammern {
 			}
 		}
 
-		// stringDurchballern(input);
 		System.out.println(mylist.toString());
 		return mylist;
 	}
 
 	private static boolean valid(String input) {
 
+		// filter: ((a))
+		for (int i = 0; i < input.length(); i++)
+			if (input.charAt(i) == '(')
+				if (input.charAt(i + 1) == '(')
+					if (input.charAt(i + 3) == ')')
+						if (input.charAt(i + 4) == ')')
+							return false;
+
 		// Keine Eintraege mit nutzlosen Klammern
 		if (!input.contains("()"))
+			if(!input.contains(")))"))
+				if(!input.contains("((("))
 			// Keine Eintraege welche bereits in der Liste enthalten sind
 			if (!mylist.contains(input))
-				return true;
+			return true;
 
 		return false;
 
