cbb8313685714f8319d0f12c05dbbd2d118f0219
Peter Henniger <Peter.Henniger@proleit.com>
2017-01-23 14:50:19 +0100

    First Version of treeDetector, with own implementation of isBinarySearchTree

--- uebung11/TreeDetector.java
+++ uebung11/TreeDetector.java
@@ -1,12 +1,84 @@
 public class TreeDetector {
 
 	public boolean isHeap(Node node) {
-		//TODO
-		return false;
+		// MaxHeap
+		// Left < Value > Right
+		// Left < Right
+
+		// Sonderfall Teilaufgabe c
+		if (node == null)
+			return true;
+
+		Node l = node.getLeftChild();
+		Node r = node.getRightChild();
+
+		// Sonderfall Teilaufgabe c
+		if (l == null && r == null)
+			return true;
+
+		// Wenn rechts groesser ist, ist es kein Max Heap
+		if (l != null) {
+			if (l.getValue().compareTo(node.getValue()) > 0)
+				return false;
+
+			// Ist die linke Seite kein Max Heap, haben wir keinen Max Heap
+			if (!isHeap(l))
+				return false;
+		}
+
+		// Wenn links groesser ist, ist es kein Max Heap
+		if (r != null) {
+			if (r.getValue().compareTo(node.getValue()) > 0)
+				return false;
+
+			// Ist die rechte Seite kein Max Heap, haben wir keinen Max Heap
+			if (!isHeap(r))
+				return false;
+		}
+
+		// if (l != null && r != null)
+		// if (l.getValue().compareTo(r.getValue()) > 0)
+		// return false;
+
+		return true;
 	}
 
 	public boolean isBinarySearchTree(Node node) {
-		//TODO
-		return false;
+		// Sonderfall Teilaufgabe c
+		if (node == null)
+			return true;
+
+		Node l = node.getLeftChild();
+		Node r = node.getRightChild();
+
+		// Sonderfall Teilaufgabe c
+		if (l == null && r == null)
+			return true;
+
+		// Wenn links groesser ist, ist es kein Binaerer Suchbaum
+		if (l != null) {
+			if (l.getValue().compareTo(node.getValue()) >= 0)
+				return false;
+
+			// Ist die linke Seite kein Max Heap, haben wir keinen Max Heap
+			if (!isBinarySearchTree(l))
+				return false;
+		}
+
+		// Wenn rechts kleiner ist, ist es kein Binaerer Suchbaum
+		if (r != null) {
+			if (r.getValue().compareTo(node.getValue()) <= 0)
+				return false;
+
+			// Ist die rechte Seite kein Max Heap, haben wir keinen Max Heap
+			if (!isBinarySearchTree(r))
+				return false;
+		}
+
+		if (l != null && r != null)
+			if (l.getValue().compareTo(r.getValue()) >= 0)
+				return false;
+
+		return true;
 	}
 }
