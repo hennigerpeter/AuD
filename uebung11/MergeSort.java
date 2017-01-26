b7374e5e4ed09bcbac81f62c3d0fd1ef061f0ae4
Peter Henniger <Peter.Henniger@proleit.com>
2017-01-26 23:11:11 +0100

    began with MergeSort

--- uebung11/MergeSort.java
+++ uebung11/MergeSort.java
@@ -9,8 +9,45 @@ public class MergeSort {
 		return null;
 	}
 
-	public List merge(List left, List right) {
+	public static List merge(List left, List right) {
 		// TODO
-		return null;
+		if (left == null)
+			return new List(null);
+
+		if (right == null)
+			return left;
+
+		Element currentL = left.first;
+		Element nextL = currentL.next;
+		Element r1 = right.first;
+
+		while (r1 != null) {
+			while (currentL.getValue() > r1.getValue() && nextL != null) {
+
+				currentL = nextL;
+				nextL = currentL.next;
+			}
+
+			// Reissverschluss
+			currentL.next = r1;
+			currentL.next.next = nextL;
+
+			r1 = r1.next;
+		}
+		// Right List doesnt get smaller -> stack overflow
+
+		return left;
+	}
+
+	public static void main(String[] args) {
+		List left = new List(new Element(7, null));
+		left.first = new Element(4, left.first);
+		left.first = new Element(2, left.first);
+
+		List right = new List(new Element(6, null));
+		right.first = new Element(3, right.first);
+
+		List merge = merge(left, right);
+
 	}
 }
