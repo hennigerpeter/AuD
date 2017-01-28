c532caf49bf42a69dc09e3e02accf6c01f8152ce
Peter Henniger <henniger.peter@gmail.com>
2017-01-28 21:12:28 +0100

    MergeSort

--- uebung11/MergeSort.java
+++ uebung11/MergeSort.java
@@ -9,77 +9,56 @@ public class MergeSort {
 		return null;
 	}
 
-	public List merge(List left, List right) {
+	public static List merge(List left, List right) {
 		// TODO
 
-		// int l =0;
-		// Element currentRight = right.first;
-		// Element currentLeft = getItem(l, left);
-		//
-		// while (currentRight != null) { // Reissverschluss
-		// currentRight = right.first;
-		// currentLeft = getItem(l, left);
-		//
-		// if (currentLeft.getValue() <= currentRight.getValue() &&
-		// currentLeft.next.getValue() > currentRight.getValue()) {
-		//
-		// Element temp = new Element(currentLeft.getValue(),currentLeft.next);
-		// currentLeft.next = currentRight;
-		// currentLeft.next.next = temp;
-		//
-		// Element temp2 = new Element(right.first.next.getValue(),
-		// right.first.next.next);
-		// right.first = temp2;
-		// }
-		// }
-		//
-		// return left;
+		Element currentRight = new Element(right.first.getValue(), right.first.next);
+		Element currentLeft = new Element(left.first.getValue(), left.first.next);
 
-		
-		
-	}
+		while (currentRight.next != null) { // Reissverschluss
 
-	private List mergeSort(List list){
-		if (list.size() <= 1)
-			return list;
-		
-		// divide and conquer
-		int mitte = list.size()/2;
-		List links = mergeSort();
-		
-	}
-	
-	private List split(int i, List list){
-		
-		Element last = new Element(list.first.getValue(), list.first.next);
-		
-		for (int x = 0; x < i; x++){
-			last = last.next;
-		}
-		
-		List temp = new List(new Element(last.getValue(), null));
-		
-	}
-	
-	private Element getItem(int i, List list) {
-		Element elem = list.first;
+			if (currentLeft.getValue() <= currentRight.getValue()
+					&& currentLeft.next.getValue() > currentRight.getValue()) {
 
-		for (int n = 0; n < i; n++) {
-			elem = elem.next;
+				// Linke Liste verlaengern
+				Element temp = new Element(currentLeft.next.getValue(), currentLeft.next.next);
+				currentLeft.next = currentRight;
+				currentRight.next = temp;
+
+				// Rechte Liste verkuerzen
+				 Element temp2 = new Element(right.first.getValue(),
+				 right.first.next);
+				 right.first = new Element(right.first.next.getValue(),null);
+			}
+
+			// naechste Elemente
+			currentLeft = new Element(currentLeft.next.getValue(), currentLeft.next.next);
+			if (currentRight.next != null)
+				currentRight = new Element(currentRight.next.getValue(), currentRight.next.next);
+			else
+				currentRight = new Element(currentRight.next.getValue(), null);
 		}
 
-		return elem;
+		return left;
 	}
 
 	public static void main(String[] args) {
-		List left = new List(new Element(7, null));
-		left.first = new Element(4, left.first);
-		left.first = new Element(2, left.first);
+//		List left = new List(new Element(7, null));
+//		left.first = new Element(4, left.first);
+//		left.first = new Element(2, left.first);
+//
+//		List right = new List(new Element(6, null));
+//		right.first = new Element(3, right.first);
+
+		List left = new List(new Element(11, null));
+		left.first = new Element(8, left.first);
+		left.first = new Element(3, left.first);
 
 		List right = new List(new Element(6, null));
 		right.first = new Element(3, right.first);
-
-		// List merge = merge(left, right);
+		
+		
+		List merge = merge(left, right);
 
 	}
 }
