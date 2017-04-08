a62cb09acfa20279f3ecf02403eaf653e28ee7ef
Peter Henniger <henniger.peter@gmail.com>
2017-04-08 17:09:27 +0200

    ADT (53/120)

--- Klausurvorbereitung/Klausur SS2016/LangtonAntJava.java
+++ Klausurvorbereitung/Klausur SS2016/LangtonAntJava.java
@@ -10,29 +10,18 @@ public class LangtonAntJava {
 			// raster ist weiß
 			d = (d+3) % 4;
 			raster[x][y] = true;
-			
-			switch (d){
-				case 0: y++; break;
-				case 1: x--; break;
-				case 2: y--; break;
-				case 3: x++; break;
-			}
-			
-			
-		} else{
+		} else {
 			// raster ist schwarz
 			d = (d+1) % 4;
 			raster[x][y] = false;
-			
-			switch (d){
+		}
+		switch (d){
 			case 0: y++; break;
 			case 1: x--; break;
 			case 2: y--; break;
 			case 3: x++; break;
+		}
 			
-		}
-		}
-		
 	}
 	
 }
